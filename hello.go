f00e5763b296e68c7e17423dd19f3859be0f7d6c
antonimassomola <antonimassomola@gmail.com>
2016-09-21 12:29:11 +0200

    Added Example & godoc instructions

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package hello
 
-func hello(i string) string {
+func Hello(i string) string {
 	o := "Hello "
 	o += i
 	o += "!"
 	return o
+}
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	return sum
 }
--- hello_test.go
+++ hello_test.go
@@ -1,11 +1,33 @@
 package hello
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestHello(t *testing.T) {
 	expected := "Hello Go!"
-	actual := hello("Go")
+	actual := Hello("Go")
 	if actual != expected {
 		t.Errorf("Test failed, expected: '%s', got: '%s'", expected, actual)
 	}
 }
+
+func TestSum(t *testing.T) {
+	expected := 15
+	actual := Sum([]int{1,2,3,4,5})
+	if actual != expected {
+		t.Errorf("Test failed, expected: %v, got: %v", expected, actual)
+	}
+}
+
+func ExampleSum() {
+	numbers := []int{5,5,5}
+	fmt.Println(Sum(numbers))
+	// Output:
+	// 15
+}
+
+// To generate documentation:
+// Run godoc: godoc -http=:6060
+// godoc -html github.com/antonimassomola/go-tdd > docs.html
